800c30e2a259c6a01968b146b4752f20ab9bf8f2
Sciroccogti <scirocco_gti@yeah.net>
2021-03-25 00:28:38 +0800

    add 0325 plot

--- scripts/plot.py
+++ scripts/plot.py
@@ -6,9 +6,9 @@ plt.xlabel(r"$E_b/N_0$")
 plt.ylabel(r"$BER$")
 plt.yscale("log")
 
-SNR = [0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0]
-BER = [1.15e-01, 1.07e-01, 9.58e-02, 8.35e-02, 7.04e-02, 6.02e-02, 4.18e-02, 1.05e-02, 2.50e-04]
-BER2 = [1.04e-01, 9.05e-02, 7.90e-02, 6.67e-02, 5.53e-02, 4.37e-02, 2.79e-02, 4.49e-03, 3.31e-05]
+SNR = [0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0]
+BER = [9.75e-02, 8.43e-02, 7.17e-02, 6.00e-02, 4.99e-02, 4.18e-02, 3.36e-02]
+BER2 = [1.01e-01, 8.81e-02, 7.63e-02, 6.42e-02, 5.24e-02, 4.15e-02, 2.67e-02]
 plt.plot(SNR, BER, color="blue", marker="o", markerfacecolor="none", label="mine")
 plt.plot(SNR, BER2, color="red", marker="o", markerfacecolor="none", label="aff3ct")
 plt.legend()
